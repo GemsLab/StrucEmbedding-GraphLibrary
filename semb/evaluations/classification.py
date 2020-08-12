34ed879177f37ae3837dff52a12f192ba1f76b43
kinmark <kinmark@umich.edu>
2020-08-12 20:16:40 +0800

    clustering

--- semb/evaluations/classification.py
+++ semb/evaluations/classification.py
@@ -1,21 +1,37 @@
 from ..exceptions import UnimplementedException, MethodKeywordUnAllowedException
+
 import networkx as nx
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import LabelBinarizer
+import sklearn.model_selection
+
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import accuracy_score
-import sklearn.model_selection
-from sklearn.linear_model import LogisticRegression
-import numpy as np
-from sklearn import metrics
 
+def multiclass_roc_auc_score(truth, pred, average="micro"):
+    lb = LabelBinarizer()
+    lb.fit(truth)
+    truth = lb.transform(truth)
+    return roc_auc_score(truth, pred, average=average)
 
 def perform_classification(dict_embeddings, dict_labels, classifier='LogisticRegression', **kwargs):
     """
-    Read in the label file for the downstreaming tasks
+    Perform classification as per the input request
 
     Arguments:
     dict_embeddings {dict} -- the output embeddings from the methods
     dict_labels {dict} -- the labels from the get_label method
-
+    classifier {string} -- the classifier to be used for performing classification.
+                           Please choose from ['LogisticRegression', 'SVM', 'KNN']
+                           
     Return:
     dict_performance {dict}
 
@@ -25,6 +41,9 @@ def perform_classification(dict_embeddings, dict_labels, classifier='LogisticReg
     Currently SEMB only supports single label classification and clustering. Multi-label classification
     and clustering will come out in the following versions.
     """
+    # Error Handling
+    if classifier not in ['LogisticRegression', 'SVM', 'KNN']:
+        raise MethodKeywordUnAllowedException("Please choose classifier from ['LogisticRegression', 'SVM', 'KNN']!")
 
     list_nodes_embeddings = set([i for i, _ in dict_embeddings.items()])
     list_nodes_labels = set([i for i, _ in dict_labels.items()])
@@ -52,28 +71,54 @@ def perform_classification(dict_embeddings, dict_labels, classifier='LogisticReg
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
         # TODO: Add more classifiers here
-        clf = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial', penalty='l2', C=1.0)
+        clf = OneVsRestClassifier(
+            LogisticRegression(
+                random_state=0, 
+                solver='lbfgs', 
+                penalty='l2', 
+                C=1.0, n_jobs=-1), 
+            n_jobs=-1)
+        if classifier == 'SVM':
+            clf = OneVsRestClassifier(
+                svm.LinearSVC(
+                    penalty='l2', 
+                    C=1.0, 
+                    multi_class='ovr'), 
+                n_jobs=-1)
+        elif classifier == 'KNN':
+            clf = KNeighborsClassifier(
+                n_neighbors=5, 
+                metric='euclidean', 
+                n_jobs=-1)
+
         clf.fit(X_train, y_train)
         y_pred = clf.predict(X_test)
 
-        acc = metrics.accuracy_score(y_test, y_pred)
-        f1_micro = metrics.f1_score(y_test, y_pred, average='micro')
-        f1_macro = metrics.f1_score(y_test, y_pred, average='macro')
+        acc = round(metrics.accuracy_score(y_test, y_pred), 4)
+        f1_micro = round(metrics.f1_score(y_test, y_pred, average='micro'), 4)
+        f1_macro = round(metrics.f1_score(y_test, y_pred, average='macro'), 4)
         dict_ = {'accuracy': acc, 'f1_macro': f1_macro, 'f1_micro': f1_micro}
+
+        if classifier in ['SVM', 'LogisticRegression']:
+            y_scores = clf.decision_function(X_test)
+            roc_auc_micro = multiclass_roc_auc_score(y_test, y_scores, average='micro')
+            roc_auc_macro = multiclass_roc_auc_score(y_test, y_scores, average='macro')
+            dict_['auc_micro'] = round(roc_auc_micro, 4)
+            dict_['auc_macro'] = round(roc_auc_macro, 4)
         dict_performance_[idx] = dict_
 
     dict_overall = dict()
     for cur_metric in ['accuracy', 'f1_macro', 'f1_micro']:
         dict_ = dict()
-        dict_['mean'] = np.mean([dict_performance_[i][cur_metric] for i in range(5)])
-        dict_['std'] = np.std([dict_performance_[i][cur_metric] for i in range(5)])
+        dict_['mean'] = round(np.mean([dict_performance_[i][cur_metric] for i in range(5)]), 4)
+        dict_['std'] = round(np.std([dict_performance_[i][cur_metric] for i in range(5)]), 4)
         dict_overall[cur_metric] = dict_
 
+    if classifier in ['SVM', 'LogisticRegression']:
+        for cur_metric in ['auc_micro', 'auc_macro']:
+            dict_ = dict()
+            dict_['mean'] = round(np.mean([dict_performance_[i][cur_metric] for i in range(5)]), 4)
+            dict_['std'] = round(np.std([dict_performance_[i][cur_metric] for i in range(5)]), 4)
+            dict_overall[cur_metric] = dict_
+
     return {"overall": dict_overall, "detailed": dict_performance_}
-
-
-
-
-
-
-
---
+++ semb/evaluations/clustering.py
@@ -0,0 +1,70 @@
+from ..exceptions import UnimplementedException, MethodKeywordUnAllowedException
+
+import networkx as nx
+import numpy as np
+
+from sklearn.cluster import KMeans
+
+from sklearn.metrics.cluster import normalized_mutual_info_score
+from sklearn import metrics
+
+
+def purity_score(y_true, y_pred):
+    # compute contingency matrix (also called confusion matrix)
+    contingency_matrix = metrics.cluster.contingency_matrix(y_true, y_pred)
+    # return purity
+    return np.sum(np.amax(contingency_matrix, axis=0)) / np.sum(contingency_matrix)
+
+
+def kmeans_best_result(X, y, n_clusters):
+    list_purity = list()
+    list_nmi = list()
+    
+    kmeans = KMeans(n_clusters=n_clusters, n_init=min(len(y), 1000), init='k-means++').fit(X)
+    
+    list_purity += [purity_score(y, kmeans.labels_)]
+    list_nmi += [normalized_mutual_info_score(y, kmeans.labels_)]
+    return {'purity': list_purity, 'nmi': list_nmi}
+
+
+def perform_clustering(dict_embeddings, dict_labels, **kwargs):
+    """
+    Perform clustering as requested
+    The number of clusters is got from the number of number of distinct labels
+
+    Arguments:
+    dict_embeddings {dict} -- the output embeddings from the methods
+    dict_labels {dict} -- the ground truth labels from the get_label method
+                           
+    Return:
+    dict_performance {dict}
+
+    ***
+    Special Notice
+    ***
+    Currently SEMB only supports single label classification and clustering. Multi-label classification
+    and clustering will come out in the following versions.
+    """
+
+    list_nodes_embeddings = set([i for i, _ in dict_embeddings.items()])
+    list_nodes_labels = set([i for i, _ in dict_labels.items()])
+    list_nodes_intersection = list_nodes_labels
+
+    if list_nodes_embeddings != list_nodes_labels:
+        print("Warning: Nodes from the input embeddings and the input labels mismatch!")
+        print("Perform classification on the intersection.")
+        list_nodes_intersection = list_nodes_embeddings.intersection(list_nodes_labels)
+
+    X = list()
+    y = list()
+    for cur_node in list_nodes_intersection:
+        X += [dict_embeddings[cur_node]]
+        y += [dict_labels[cur_node]]
+    X = np.array(X)
+    y = np.array(y)
+
+    N_CLUSTERS = len(np.unique(y))
+
+    dict_results = kmeans_best_result(X, y, N_CLUSTERS)
+
+    return {"overall": dict_results}
