d6ae3f31ca7f663a25450aefc63e8b603c69f966
kinmark <kinmark@umich.edu>
2020-10-01 14:29:19 +0800

    add datasets and fix Example.ipynb

---
+++ semb/datasets/PPI/dataset.py
@@ -0,0 +1,36 @@
+from semb.datasets import BaseDataset, DatasetInfo
+
+import os
+import networkx as nx
+from typing import List
+
+# TODO: Make this a remote URL in the future
+SAMPLE_DATA_DIR = os.path.join(os.path.dirname(__file__), "../../../sample-data/PPI")
+
+class Dataset(BaseDataset):
+    
+    def get_id(self) -> str:
+        return 'ICEWS'
+
+    def get_datasets(self) -> List[DatasetInfo]:
+        return [
+            DatasetInfo(name="PPI", description="PPI data", \
+                    src_url=f'{SAMPLE_DATA_DIR}/PPI.edgelist')]
+
+    def load_dataset(self, dataset: DatasetInfo, directed=False, weighted=False) -> nx.Graph:
+        if weighted:
+            graph = nx.read_edgelist(
+                dataset.src_url, 
+                nodetype=int, 
+                data=(('weight', 'data')), 
+                create_using=(nx.Graph() if not directed else nx.DiGraph()))
+        else:
+            graph = nx.read_edgelist(
+                dataset.src_url,
+                nodetype=int,
+                create_using=(nx.Graph() if not directed else nx.DiGraph()))
+            for edge in graph.edges():
+                graph[edge[0]][edge[1]]['weight'] = 1
+
+        return graph
+            
